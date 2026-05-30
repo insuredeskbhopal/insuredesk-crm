f00b8bbf10504c06c94b41125f7774c122a4e598
ABHISHEK9009 <abhishek9009@users.noreply.github.com>
2026-05-30 15:51:20 +0530

    Ignore salutations in customer IDs

--- tests/records.test.js
+++ tests/records.test.js
@@ -4,7 +4,14 @@ import { buildCustomerId, normalizeRecord } from "../lib/records.js";
 describe("record customer ID", () => {
   it("uses the first four insured-name letters and last four unmasked phone digits", () => {
     expect(buildCustomerId("Leena Sajwani", "9876543210")).toBe("LEEN3210");
-    expect(buildCustomerId("M/S AD Mark Advertising", "+91 8818889660")).toBe("MSAD9660");
+    expect(buildCustomerId("AD Mark Advertising", "+91 8818889660")).toBe("ADMA9660");
+  });
+
+  it("ignores salutations before taking customer ID name letters", () => {
+    expect(buildCustomerId("Mr Sunil Kumar Shukla", "9988776655")).toBe("SUNI6655");
+    expect(buildCustomerId("Mrs Sarita Verma", "9123456789")).toBe("SARI6789");
+    expect(buildCustomerId("Ms Leena Sajwani", "9876543210")).toBe("LEEN3210");
+    expect(buildCustomerId("M/S AD Mark Advertising", "+91 8818889660")).toBe("ADMA9660");
   });
 
   it("does not build the phone suffix from masked digits", () => {
