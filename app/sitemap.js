9972091c800f1bb335c7177ff45d40f796a90592
abhis <abhis@users.noreply.github.com>
2026-06-18 14:35:35 +0530

    feat: include dynamic blog article URLs in sitemap

--- app/sitemap.js
+++ app/sitemap.js
@@ -1,10 +1,21 @@
 import { PUBLIC_ROUTES, SITE_URL } from "@/lib/seo/site";
+import { BLOG_POSTS } from "./blog/blogData";
 
 export default function sitemap() {
-  return PUBLIC_ROUTES.map((route) => ({
+  const routes = PUBLIC_ROUTES.map((route) => ({
     url: new URL(route.path, SITE_URL).href,
     lastModified: new Date(),
     changeFrequency: route.changeFrequency,
     priority: route.priority
   }));
+
+  const blogRoutes = BLOG_POSTS.map((post) => ({
+    url: `${SITE_URL}/blog/${post.slug}`,
+    lastModified: new Date(post.date),
+    changeFrequency: "monthly",
+    priority: 0.7
+  }));
+
+  return [...routes, ...blogRoutes];
 }
+
